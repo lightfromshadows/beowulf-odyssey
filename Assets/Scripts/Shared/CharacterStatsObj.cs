7b740fc216b9b6120e9b65fbece72793ee712800
Christian Plummer <lightfromshadows@gmail.com>
2018-03-15 17:27:46 -0500

    Laid out basic item database and player/wolf stats.

---
+++ Assets/Scripts/Shared/CharacterStatsObj.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterStatsObj : ScriptableObject
+{
+    [System.Serializable]
+    public struct StatsContainer
+    {
+        public float power;
+        public float hitChance;
+        public float health;
+    }
+
+    [SerializeField] 
+    private StatsContainer defaultStats = new StatsContainer() {power = 100f, hitChance = 50f, health = 80f };
+
+    public float Power {
+        get {
+            return defaultStats.power;
+        }
+    }
+
+    public float HitChance {
+        get {
+            return defaultStats.hitChance;
+        }
+    }
+
+    protected float _health = 0f;
+    public float Health {
+        get {
+            return _health; 
+        }
+        set {
+            _health = value;
+        }
+    }
+
+    public void Init()
+    {
+        _health = defaultStats.health;
+        buffs.Clear();
+    }
+
+    List<BuffItem> buffs = new List<BuffItem>();
+}
---
+++ Assets/Scripts/Shared/ItemDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemDatabase : ScriptableObject {
+    public List<BuffItem> database = new List<BuffItem>();
+}
+
+[System.Serializable]
+public struct BuffItem
+{
+    public string name;
+    [TextArea]
+    public string description;
+    public Sprite sprite;
+    public bool passive;
+    public bool consumable;
+
+    public float powerUp;
+    public float hitUp;
+    public float healthUp;
+}
