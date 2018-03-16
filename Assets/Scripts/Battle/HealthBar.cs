59df08be37730b1a4f4529e561be11f0db67e82f
Christian Plummer <lightfromshadows@gmail.com>
2018-03-16 14:25:00 -0600

    Added health bars

---
+++ Assets/Scripts/Battle/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField] CharacterStatsObj myStats;
+    [SerializeField] Slider slider;
+    [SerializeField] Image fillImage;
+    [SerializeField] Color lowHealth;
+    [SerializeField] Color highHealth;
+
+	// Update is called once per frame
+	void Update () {
+        float t = myStats.Health / myStats.MaxHealth;
+
+        fillImage.color = Color.Lerp(lowHealth, highHealth, t);
+        slider.value = t;
+	}
+}
