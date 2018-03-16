9b39d105067094385cad83ed7b80ec170dff0ee1
Michael LeSauvage <mike.lesauvage@gmail.com>
2018-03-15 20:57:48 -0400

    Mike's first crack at towns.

---
+++ Assets/Scripts/Town/Sun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+    private float dayTimeTween = 0;
+
+    private const int SUN_MIN_HEIGHT = 650;
+    private const int SUN_RISE_HEIGHT = 340;
+
+    public float DayTimeTween
+    {
+        get
+        {
+            return dayTimeTween;
+        }
+
+        set
+        {
+            dayTimeTween = value;
+
+            Vector2 pos = new Vector2();
+            pos.x = 100 + (1 - dayTimeTween) * 1720;
+
+            dayTimeTween = 1 - dayTimeTween * 2;
+ 
+            pos.y = Mathf.Sqrt(1 - dayTimeTween * dayTimeTween);
+            pos.y *= SUN_RISE_HEIGHT;
+            pos.y += SUN_MIN_HEIGHT;
+            transform.position = pos;
+ 
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Town/TownManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TownManager : MonoBehaviour {
+
+    public Sun sun;
+    private const float MAX_DAY_TIME = 10;
+    private float dayTime = 0;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        dayTime += Time.deltaTime;
+
+        if(dayTime >= MAX_DAY_TIME)
+        {
+            //Run end of day logic.
+            dayTime = 0;
+        }
+        sun.DayTimeTween = dayTime / MAX_DAY_TIME;
+    }
+}
