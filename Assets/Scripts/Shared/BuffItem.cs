dab4bad52fbabd44124e7c9d15b88312d40a6850
Christian Plummer <lightfromshadows@gmail.com>
2018-03-15 20:17:20 -0500

    Changed Item Buffs to Scriptable Objects

---
+++ Assets/Scripts/Shared/BuffItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BuffItem : ScriptableObject
+{
+    [TextArea]
+    public string description;
+    public Sprite sprite;
+    public bool passive;
+    public bool consumable;
+
+    public float powerUp;
+    public float hitUp;
+    public float healthUp;
+
+}
--- Assets/Scripts/Shared/ItemDatabase.cs
+++ Assets/Scripts/Shared/ItemDatabase.cs
@@ -6,18 +6,3 @@ using UnityEngine;
 public class ItemDatabase : ScriptableObject {
     public List<BuffItem> database = new List<BuffItem>();
 }
-
-[System.Serializable]
-public struct BuffItem
-{
-    public string name;
-    [TextArea]
-    public string description;
-    public Sprite sprite;
-    public bool passive;
-    public bool consumable;
-
-    public float powerUp;
-    public float hitUp;
-    public float healthUp;
-}
