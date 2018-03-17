4f8da384d1b650238d2a80959acd6e0e582b1868
Christian Plummer <lightfromshadows@gmail.com>
2018-03-17 11:15:00 -0600

    Added jump temp sprites Added graveyard Added tombstone Added epitaph to Townsperson

---
+++ Assets/Scripts/Town/Tombstone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Tombstone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    [SerializeField] TownsPerson person;
+    [SerializeField] Text title;
+    [SerializeField] Text epitaph;
+
+    [SerializeField] new Collider2D collider;
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        title.enabled = false;
+        epitaph.enabled = false;
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        title.enabled = true;
+        epitaph.enabled = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+        title.text = person.name;
+        epitaph.text = person.epitaph;
+
+	}
+}
