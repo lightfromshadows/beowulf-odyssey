d25f17968f58e42cc4fdce5de980b6a2ef754c9a
Michael LeSauvage <mike.lesauvage@gmail.com>
2018-03-17 16:12:22 -0400

    Added sky gradient.

---
+++ Assets/SkyGradient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyGradient : MonoBehaviour {
+
+    public Gradient gradient;
+
+    public void SetSkyColor(float percent)
+    {
+        GetComponent<SpriteRenderer>().color = gradient.Evaluate(percent);
+    }
+    
+}
