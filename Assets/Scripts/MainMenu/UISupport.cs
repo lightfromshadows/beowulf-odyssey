f6e31400750bcae6c90a96abe92651564d223ae1
Christian Plummer <lightfromshadows@gmail.com>
2018-03-16 22:10:33 -0600

    Added Main Menu

--- Assets/Scripts/MainMenu/UISupport.cs
+++ Assets/Scripts/MainMenu/UISupport.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UISupport : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] string teaser = "The Abyss Looks Into You";
+    [SerializeField] float teaserTime = 1f;
+    [SerializeField] Text buttonText;
+
+    bool triggerOnce = false;
+
+    public void StartTheGame()
+    {
+        if (triggerOnce) return;
+
+        StartCoroutine(TeaseAndStart());
+    }
+
+    IEnumerator TeaseAndStart()
+    {
+        buttonText.text = teaser;
+
+        yield return new WaitForSeconds(teaserTime);
+
+        SceneManager.LoadScene("Town");
+    }
 }
